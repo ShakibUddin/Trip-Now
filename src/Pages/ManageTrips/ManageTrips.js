d8e94a73751284c5901d76ab3638e3b1d47fe8e5
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2021-10-30 01:43:16 +0600

    table in managetrips is responsive

--- src/Pages/ManageTrips/ManageTrips.js
+++ src/Pages/ManageTrips/ManageTrips.js
@@ -1,83 +1,42 @@
 import React from 'react';
+import { Table, Tbody, Td, Th, Thead, Tr } from 'react-super-responsive-table';
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 import useData from '../../Hooks/useData';
 
 const Managebookings = () => {
     const { allBookings, handleDeleteBooking } = useData();
     return (
-        <div className="w-full mx-auto flex flex-col flex-wrap justify-center p-2">
-            <div className="-my-2 overflow-x-auto">
-                <div className="py-2 align-middle inline-block min-w-full">
-                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                        <table className="min-w-full divide-y divide-gray-200">
-                            <thead className="bg-gray-50">
-                                <tr>
-                                    <th
-                                        scope="col"
-                                        className="py-3 text-center text-xs font-medium text-gray-500 uppercase "
-                                    >
-                                        Trip-Id
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                    >
-                                        Email
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                    >
-                                        Contact
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                    >
-                                        Address
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                    >
-                                        Approved
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                    >
-                                        Action
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody className="bg-white divide-y divide-gray-200">
-                                {allBookings.map((booking) => (
-                                    <tr key={booking.tripId}>
-                                        <td className="py-4 whitespace-nowrap text-center">
-                                            <div className="text-sm text-gray-900">{booking.tripId}</div>
-                                        </td>
-                                        <td className="py-4 whitespace-nowrap text-center">
-                                            <div className="text-sm text-gray-900">{booking.email}</div>
-                                        </td>
-                                        <td className="py-4 whitespace-nowrap text-center">
-                                            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                {booking.contact}
-                                            </span>
-                                        </td>
-                                        <td className="py-4 whitespace-nowrap text-center text-sm text-gray-500">{booking.address}</td>
-                                        <td className="py-4 whitespace-nowrap text-center text-sm text-gray-500">{booking.approved ? <p>Yes</p> : <p>No</p>}</td>
-                                        <td className="py-4 whitespace-nowrap text-center text-sm font-medium">
-                                            <div className="flex justify-around">
-                                                <button className="w-2/4 mx-2 p-2 text-white text-xs bg-green-500">Approve</button>
-                                                <button className="w-2/4 mx-2 p-2 text-white text-xs bg-red-500" onClick={() => handleDeleteBooking(booking._id)}>Delete</button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+        <div className="w-full p-2">
+            <Table>
+                <Thead>
+                    <Tr className="bg-blue-600">
+                        <Th className="text-center text-white font-bold text-sm uppercase py-3">TripId</Th>
+                        <Th className="text-center text-white font-bold text-sm uppercase py-3">Email</Th>
+                        <Th className="text-center text-white font-bold text-sm uppercase py-3">Contact</Th>
+                        <Th className="text-center text-white font-bold text-sm uppercase py-3">Address</Th>
+                        <Th className="text-center text-white font-bold text-sm uppercase py-3">Approved</Th>
+                        <Th className="text-center text-white font-bold text-sm uppercase py-3"></Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {
+                        allBookings.map(booking =>
+                            <Tr key={booking._id} className="bg-white">
+                                <Td className="text-gray-400 text-xs text-center py-3">{booking.tripId}</Td>
+                                <Td className="text-gray-400 text-xs text-center py-3">{booking.email}</Td>
+                                <Td className="text-gray-400 text-xs text-center py-3">{booking.contact}</Td>
+                                <Td className="text-gray-400 text-xs text-center py-3">{booking.address}</Td>
+                                <Td className="text-gray-400 text-xs text-center py-3">{booking.approved ? "Yes" : "No"}</Td>
+                                <Td>
+                                    <div className="flex justify-center">
+                                        <button className="w-2/4 mx-1 p-2 bg-green-500 text-white">Approve</button>
+                                        <button className="w-2/4 mx-1 p-2 bg-red-500 text-white" onClick={() => handleDeleteBooking(booking._id)}>Delete</button>
+                                    </div>
+                                </Td>
+                            </Tr>)
+                    }
+                </Tbody>
+            </Table>
         </div>
     );
 };
