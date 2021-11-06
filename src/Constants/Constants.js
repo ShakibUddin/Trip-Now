136f55af566b0431175b3d3f17bf99d469232255
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2021-11-06 22:20:21 +0600

    apis are now connected to remote server

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,4 +1,4 @@
-const productionUrl = 'https://secure-hollows-37742.herokuapp.com';
+const productionUrl = 'https://trip-now.herokuapp.com/';
 const developmentUrl = 'http://localhost:5000';
 
 module.exports = { productionUrl, developmentUrl };
